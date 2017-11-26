f8898a7129c84aac6fec63e05e4e7d0f4c094e39
Unknown <ogjrivero@gmail.com>
2017-11-26 18:43:38 -0400

    added graphs

--- jq_script.js
+++ jq_script.js
@@ -39,6 +39,35 @@ $(document).ready(function () {
         createNewApp(id, database);
     });
 
+    $("body").on("click", ".addValue", function () {
+        let attribute = $(this).parent();
+        // console.log($(this).parent().text());
+        // console.log($(this).parent().children("input[type='number']").val());
+
+        database.ref('users/'+id.val()+'/list/'+attribute.text()).once('value').then(function (snap) {
+
+            if (!$("#"+attribute.text()+"Graph").length) {
+                console.log("DONE");
+                $("#"+attribute.text()).append("<div id='"+attribute.text()+"Graph' style='height:300px'></div>"+"</div>");
+            }
+
+            let list = [];
+            list = snap.val();
+            console.log(list);
+            list.push({
+                date: new Date().getSeconds()+"",
+                value: parseInt(attribute.children("input[type='number']").val()),
+            });
+            attribute.children("input[type='number']").val("");
+            createGraph(attribute.text()+"Graph", list);
+            // console.log(attribute.text());
+            // console.log(list);
+            database.ref('users/'+id.val()+'/list/'+attribute.text()).set(list);
+            
+        })
+           
+    });
+
 });
 
 function createNewApp(id, database) {
@@ -46,31 +75,23 @@ function createNewApp(id, database) {
     //     list: $("select").val(),
     // });
 
-    $("body").on("click", ".addValue", function () {
-        console.log($(this).parent().text());
-        // let updates = {};
-        // updates['/users/'+id.val()+'/list/'+$(this).parent().text()+'/'] = 7;
-        // database.ref().update(updates);            
-    })
-
     database.ref('users/'+id.val()+'/list').once('value').then(function (snap) {
         // console.log(snap.val().hasOwnProperty($("select").val()));
         if (!snap.val() || !snap.val().hasOwnProperty($("select").val())){
             let updates = {};
-            updates['/users/'+id.val()+'/list/'+$("select").val()+'/'] = "lol";
+            updates['/users/'+id.val()+'/list/'+$("select").val()+'/'] = [{
+                date: 4+"",
+                value: 3,
+            }];
             database.ref().update(updates);
-            $("#states").append("<div class=''>"+
-                "<h2>"+$("select").val()+"<input type='button' value='+' class='addValue'></h2>"+
+            $("#states").append("<div class='' id="+$("select").val()+">"+
+                "<h2>"+$("select").val()+"<input type='button' value='+' class='addValue'>"+
+                "<input type='number' placeholder='Agregar dato'></h2>"+
             "</div>");
         }
         
     })
 
-    
-    // console.log($("select").val());
-    // $(".addValue").click(function () {
-    //     console.log("LOLI");
-    // })
 }
 
 function loadDB() {
@@ -94,6 +115,7 @@ function logInButton(database, id) {
             // console.log(snap.val()[id.val()]);
             $("#login").hide();
             $("#states").show();
+            $("#container").css("padding-top","50px");
             retrieveDB(snap.val()[id.val()]);
         }
         else {
@@ -107,11 +129,40 @@ function retrieveDB (personDB) {
     // console.log(personDB.list);
     if (personDB.list) {
         for (key of Object.keys(personDB.list)) {
-            $("#states").append("<div class=''>"+
-                "<h2>"+key+"<input type='button' value='+' class='addValue'></h2>"+
+            $("#states").append("<div class='' id="+key+">"+
+                "<h2>"+key+"<input type='button' value='+' class='addValue'>"+
+                "<input type='number' placeholder='Agregar dato'></h2>"+
+                "<div id="+key+"Graph"+" style='height:300px'></div>"+
             "</div>");
+            // console.log(typeof(key));
+            // console.log(personDB.list.hasOwnProperty(key));
+            // console.log(personDB.list[key]);
+            // createGraph(key, [
+            //     {a: ""+5, b: 6},
+            //     {a: ""+3, b: 74}
+            // ]);
+            createGraph(key+"Graph", personDB.list[key]);
         }
     }
+}
+
+function createGraph(attribute, values) {
+    $("#"+attribute).html("");
+    //console.log(values);
+    new Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: attribute,
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: values,
+        // The name of the data record attribute that contains x-values.
+        xkey: 'date',
+        // A list of names of data record attributes that contain y-values.
+        ykeys: ['value'],
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['Value']
+      });
 }
 
 function registerButton(database, id) {
